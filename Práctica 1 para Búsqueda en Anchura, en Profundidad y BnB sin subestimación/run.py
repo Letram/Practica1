57df19fc807a8cb9e969c6141b693b6daa614e75
Letram <carlos.martel105@alu.ulpgc.es>
2017-05-10 15:15:15 +0100

    Added BnB search and the other 2

---
+++ Práctica 1 para Búsqueda en Anchura, en Profundidad y BnB sin subestimación/run.py
@@ -0,0 +1,19 @@
+# coding=utf-8
+# Search methods
+
+import search
+                    #hasta y desde
+ab = search.GPSProblem('O', 'S', search.romania)
+
+print "------------------------------anchura---------------------------------"
+print search.breadth_first_graph_search(ab).path()
+print "----------------------------profundidad-------------------------------"
+print search.depth_first_graph_search(ab).path()
+print "--------------------------Branch&Bound--------------------------------"
+print search.branch_bound_graph_search(ab).path()
+
+#print search.astar_search(ab).path()
+
+# Result:
+# [<Node B>, <Node P>, <Node R>, <Node S>, <Node A>] : 101 + 97 + 80 + 140 = 418
+# [<Node B>, <Node F>, <Node S>, <Node A>] : 211 + 99 + 140 = 450
